7e9a0b010fbe2a6be9a7616ed9edaff1dae38718
limcheekeen <limcheekeen.63@gmail.com>
2024-11-22 12:54:01 +0800

    Updates

--- src/constants.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Type for all event logs
- */
-export interface RealtimeEvent {
-    time: string;
-    source: 'client' | 'server';
-    count?: number;
-    event: { [key: string]: any };
-  }
